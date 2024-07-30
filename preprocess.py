19c5a2a3967dffddd1f4070f971adf43c28a50be
Omar Choudhry <omar.choudhry1@hotmail.com>
2024-07-30 18:44:03 +0100

    Added v10m

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,6 @@
 import os
 
-models = ["n", "s", "b", "l", "x"]
+models = ["n", "s", "m", "b", "l", "x"]
 
 for m in models:
-    os.system(f'python YOLO.py > "chkpts/ART/yolov10{m}-conf.txt"')
+    os.system(f'python YOLO.py > "chkpts/ART/v10{m}/yolov10{m}-conf.txt"')
