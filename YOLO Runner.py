97b5dbb67329bb9af4accd2492a8bfc0201261df
Omar Choudhry <omar.choudhry1@hotmail.com>
2024-08-06 21:20:26 +0100

    Fixed error

--- YOLO Runner.py
+++ YOLO Runner.py
@@ -3,6 +3,7 @@ import os
 models = ["n", "s", "m", "b", "l", "x"]
 
 for m in models:
-    os.mkdir(f"chkpts/6DOF/v10{m}")
+    if not os.path.exists(f"chkpts/6DOF/v10{m}"):
+        os.mkdir(f"chkpts/6DOF/v10{m}")
     os.system(f'python "YOLO 6DOF".py > "chkpts/6DOF/v10{m}/yolov10{m}-conf.txt"')
     # os.system(f'python YOLO.py > "chkpts/ART/v10{m}/yolov10{m}-conf.txt"')
