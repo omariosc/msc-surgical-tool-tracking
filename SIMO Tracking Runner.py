2b3035522ffd773e030a7f4815d9f75d64ab0e2f
Omar Choudhry <omar.choudhry1@hotmail.com>
2024-08-12 13:24:24 +0100

    Ran for 6DOF

--- SIMO Tracking Runner.py
+++ SIMO Tracking Runner.py
@@ -1,9 +1,9 @@
 import os
 
-data = ["ART", "6DOF"]
-
+# data = ["ART", "6DOF"]
+data = ["6DOF"]
 for d in data:
     # arch = ["vgg", "resnet18", "resnet50"]
     arch = "vgg"
-    os.makedirs(f"chkpts/{d}/{arch}", exist_ok=True)
+    os.makedirs(f"chkpts/SIMO/{d}", exist_ok=True)
     os.system(f'python "SIMO Tracking".py {d} {arch}> "chkpts/SIMO/{d}/{arch}.txt"')
