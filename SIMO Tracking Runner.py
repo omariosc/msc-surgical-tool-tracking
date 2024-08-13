b28c019a4108da9c1c768e4c47a62f7a1dacae34
Omar Choudhry <omar.choudhry1@hotmail.com>
2024-08-13 23:59:12 +0100

    Running models

--- SIMO Tracking Runner.py
+++ SIMO Tracking Runner.py
@@ -1,9 +1,11 @@
+
 import os
 
 data = ["ART", "6DOF"]
-# data = ["6DOF"]
 for d in data:
     # arch = ["vgg", "resnet18", "resnet50"]
     arch = "fcn"
-    os.makedirs(f"chkpts/SIMO/{d}", exist_ok=True)
-    os.system(f'python "SIMO Tracking".py {d} {arch}> "chkpts/SIMO/{d}/{arch}.txt"')
+    os.makedirs(f"chkpts/SIMO/{d}/{arch}", exist_ok=True)
+    os.system(
+        f'python "SIMO Tracking".py {d} {arch}> "chkpts/SIMO/{d}/{arch}/{arch}.txt"'
+    )
