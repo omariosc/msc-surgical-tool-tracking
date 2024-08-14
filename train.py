678b923daaae5df2321c5183d3ee541fb72837d4
Omar Choudhry <omar.choudhry1@hotmail.com>
2024-08-14 08:08:12 +0100

    Running YOLOv8 for 6DOF and ART

--- train.py
+++ train.py
@@ -1,12 +1,14 @@
 import os
 
-# models = ["n", "s", "m", "b", "l", "x"]
-models = ["n", "x"]
+models = ["n", "s", "m", "b", "l", "x"]
+models = ["n", "s", "m", "l", "x"]
 
 for m in models:
-    if not os.path.exists(f"chkpts/6DOF/v10{m}"):
-        os.mkdir(f"chkpts/6DOF/v10{m}")
-    if not os.path.exists(f"chkpts/ART/v10{m}"):
-        os.mkdir(f"chkpts/ART/v10{m}")
-    os.system(f'python "YOLO 6DOF".py "{m}" > "chkpts/6DOF/v10{m}/yolov10{m}-train.txt"')
-    # os.system(f'python YOLO.py "{m}" > "chkpts/ART/v10{m}/yolov10{m}-val.txt"')
+    if not os.path.exists(f"chkpts/6DOF/v8{m}"):
+        os.mkdir(f"chkpts/6DOF/v8{m}")
+    os.system(f'python "YOLO 6DOF".py "{m}" > "chkpts/6DOF/v8{m}/yolov8{m}.txt"')
+
+for m in models:
+    if not os.path.exists(f"chkpts/ART/v8{m}"):
+        os.mkdir(f"chkpts/ART/v8{m}")
+    os.system(f'python YOLO.py "{m}" > "chkpts/ART/v8{m}/yolov8{m}.txt"')
