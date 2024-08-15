1626e815dc3c7b8bb52ab8fa8c2984e901d8f49d
Omar Choudhry <omar.choudhry1@hotmail.com>
2024-08-15 11:42:30 +0100

    Need to run two more models

--- train.py
+++ train.py
@@ -1,12 +1,12 @@
 import os
 
 models = ["n", "s", "m", "b", "l", "x"]
-models = ["n", "s", "m", "l", "x"]
+models = ["l", "x"]
 
-for m in models:
-    if not os.path.exists(f"chkpts/6DOF/v8{m}"):
-        os.mkdir(f"chkpts/6DOF/v8{m}")
-    os.system(f'python "YOLO 6DOF".py "{m}" > "chkpts/6DOF/v8{m}/yolov8{m}.txt"')
+# for m in models:
+#     if not os.path.exists(f"chkpts/6DOF/v8{m}"):
+#         os.mkdir(f"chkpts/6DOF/v8{m}")
+#     os.system(f'python "YOLO 6DOF".py "{m}" > "chkpts/6DOF/v8{m}/yolov8{m}.txt"')
 
 for m in models:
     if not os.path.exists(f"chkpts/ART/v8{m}"):
