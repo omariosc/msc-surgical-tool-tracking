3e542f969f65a8352c2cfc18a374804b429c3f06
Omar Choudhry <omar.choudhry1@hotmail.com>
2024-08-19 08:20:06 +0100

    Renamed

--- src/SIMO Tracking Runner.py
+++ src/SIMO Tracking Runner.py
@@ -5,7 +5,7 @@ data = ["ART", "6DOF"]
 for d in data:
     arch = ["resnet50", "vgg", "fcn-resnet", "fcn-vgg"]
     for a in arch:
-        if (d == "6DOF" and (a == "fcn-resnet" or a == "vgg")) or (d == "ART" and (a != "resnet50")):
+        if (d == "6DOF" and (a == "fcn-resnet" or a == "vgg")) or (d == "ART"):
             continue
         print(d, a)
         os.makedirs(f"chkpts/SIMO2/{d}/{a}", exist_ok=True)
